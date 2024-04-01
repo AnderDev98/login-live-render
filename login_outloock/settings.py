d8e37c36be3f7b30f869e32fe6ba642ea5f7a9c5
anderdev98 <andersonmendoza186@gmail.com.com>
2024-03-31 21:12:09 -0400

    allowed host full 2

--- login_outloock/settings.py
+++ login_outloock/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # DEBUG = 'RENDER' not in os.environ
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://login-live-qx1x.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
